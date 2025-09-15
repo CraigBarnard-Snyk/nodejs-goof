6ef7b6166fbb2dc3740320bd3115905bba8e6961
Craig Barnard <craig.barnard@snyk.io>
2025-09-15 15:58:02 +1200

    Feat: add xss vuln

--- routes/users.js
+++ routes/users.js
@@ -48,4 +48,4 @@ router.get('/xss', (req, res) => {
   const name = req.query.name || 'world';
   // Vulnerable: unsanitized user input is rendered directly in HTML 
   res.send(`<h1>Hello ${name}</h1>`);
-});
+}); 
