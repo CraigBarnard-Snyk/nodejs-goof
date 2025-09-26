d6c70946714cba3e846dd5327edf3522da6381c5
Craig Barnard <craig.barnard@snyk.io>
2025-09-26 19:32:53 +1200

    demo: add xss vuln

--- routes/users.js
+++ routes/users.js
@@ -43,3 +43,10 @@ router.post('/', async (req, res, next) => {
     next();
   }
 })
+
+
+router.get('/xss', (req, res) => {
+  const name = req.query.name || 'world';
+  // Vulnerable: unsanitized user input is rendered directly in HTML document
+  res.send(`<h1>Hello ${name}</h1>`);
+});
