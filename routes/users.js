de07766824dd52a4e3ec2e2a3c551f0024ad18f9
CraigBarnard-Snyk <craig.barnard@snyk.io>
2025-10-03 18:10:05 +1300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res, next) => {
 
   // hard-coded getting account id of 1
   // as a rpelacement to getting this from the session and such
-  // (just imagine that we implemented auth, etc)d
+  // (just imagine that we implemented auth, etc)
   const results = await repo.find({ id: 1 })
 
   // Log Object's where property for debug reasons:
