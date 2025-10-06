eeb14791f7d9d043f7f43f17376927a205a5977d
Craig Barnard <craig.barnard@snyk.io>
2025-10-07 09:38:22 +1300

    test: try retrigger

--- routes/users.js
+++ routes/users.js
@@ -23,6 +23,12 @@ router.get('/', async (req, res, next) => {
 
 })
 
+router.get('/xss-vsds', (req, res) => {
+  const name = req.query.name || 'world';
+  // Vulnerable: unsanitized user input is rendered directly in HTML document
+  res.send(`<h1>Candylfloss ${name}</h1>`);
+});
+
 router.post('/', async (req, res, next) => {
   try {
     const mongoConnection = typeorm.getConnection('mysql')
@@ -45,8 +51,3 @@ router.post('/', async (req, res, next) => {
 })
 
 
-router.get('/xss', (req, res) => {
-  const name = req.query.name || 'world';
-  // Vulnerable: unsanitized user input is rendered directly in HTML document
-  res.send(`<h1>Hello ${name}</h1>`);
-});
